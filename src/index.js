14bde183a622d9b11b5d8235e87f72670ec7728e
muopham <muopham5116@gmail.com>
2022-03-31 17:21:58 +0700

    add Router and Controller

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
-const path = require('path');
-const app = express();
-const port = 3000;
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const path = require('path')
+const app = express()
+const port = 3000
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+const route = require('./routes/index')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+  extended:true,
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', engine({ extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, 'resources/views'));
 
+// Routes init
+route(app);
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-app.get('/tin-tuc', (req, res) => {
-  res.render('new');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
